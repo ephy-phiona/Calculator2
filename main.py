001e7bc9273dc13cd8b216badda56802441887ec
ephy-phiona <ephyphiona@gmail.com>
2022-04-24 09:42:30 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def add(x,y):
+    return x+y
+
+
+def subtract(x,y):
+    return x-y
+
+
+def multiply (x,y):
+    return x*y
+    
+def divide (x,y):
+    return x/y
+
+
+
+    print("select operation.")
+    print("Add")
+    print("Subtract")
+    print("Multiply")
+    print("Divide")   
